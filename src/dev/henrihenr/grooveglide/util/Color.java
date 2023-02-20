d1c5531037314efff6c9ae0fa34679bd6358badd
Danmyrer <danmyr09@gmail.com>
2023-02-20 14:26:04 +0100

    Initial Commit

---
+++ src/dev/henrihenr/grooveglide/util/Color.java
@@ -0,0 +1,22 @@
+package dev.henrihenr.grooveglide.util;
+
+/**
+ * Erweiterung von {@link java.awt.Color} um neue Farben.
+ */
+public class Color extends java.awt.Color
+{
+    /**
+     * Standartkonstruktor
+     */
+    public Color(int rgb)
+    {
+        super(rgb);
+    }
+
+    public static final java.awt.Color SIZZLING_RED = java.awt.Color.decode("#F2545B");
+    public static final java.awt.Color AMARANTH_PURPLE = java.awt.Color.decode("#A93F55");
+    public static final java.awt.Color GUNMETAL = java.awt.Color.decode("#19323c");
+    public static final java.awt.Color GUNMETAL_DARK = java.awt.Color.decode("#162d36");
+    public static final java.awt.Color MINT_CREAM = java.awt.Color.decode("#F3F7F0");
+    public static final java.awt.Color NON_PHOTO_BLUE = java.awt.Color.decode("#A3E7FC");
+}
